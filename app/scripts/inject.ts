ca9120164a08117709dce45e0cce02545f7b5a14
Gagan Rai <gagan_rai@outlook.com>
2018-12-09 22:02:57 +0545

    complete ad  skip feature

--- app/scripts/inject.ts
+++ app/scripts/inject.ts
@@ -1,14 +1,15 @@
 import Util from '@libs/Utils'
 
-class Main {
+class Viewer {
 
     private video: HTMLVideoElement
-    private btnSkipAd: Element
-    private videoDuration: Number
+    private videoDuration: number
+    private isAsia: boolean
+    private isAdRunning: boolean
 
     constructor() {
         this.video = <HTMLVideoElement>Util.select('.html5-main-video')
-
+        console.log('inside constructor')
         this.video.addEventListener('ended', () => {
             this.videoEnd()
         })
@@ -20,26 +21,55 @@ class Main {
     }
 
     private checkAd(): boolean {
-        if (Util.select('.ytp-ad-texts'))
+        if (Util.select('.ytp-ad-texts')) {
+            this.isAsia = true
             return true
+        }
+
+        if (Util.select('.videoAdUiAttribution')) {
+            this.isAsia = false
+            return true
+        }
 
         return false
     }
 
     private handleAd(): void {
-
+        let button = Util.select(this.isAsia ? '.ytp-ad-skip-button' : '.videoAdUiSkipButton')
+        this.isAdRunning = true
+        console.log('video duration', this.videoDuration)
+        setTimeout(() => {
+            console.log('inside ad timeout', button)
+            if (button && this.videoDuration > 15) {
+                console.log('perform-click')
+                Util.performClick(button)
+            }
+            setTimeout(() => {
+                this.isAdRunning = false
+            }, 2000)
+        }, 10000)
 
     }
 
     public initialize(): void {
+        console.log('Initialize')
         this.videoDuration = this.video.duration
         if (this.checkAd()) {
+            console.log('Found Ad.')
             this.handleAd()
         }
+        this.video.addEventListener('ended', () => {
+            this.videoEnd()
+        })
     }
 
     public videoEnd(): void {
-        location.reload(true)
+        if (!this.isAdRunning) {
+            console.log('page reload')
+            location.reload(true)
+        }
     }
 
 }
+
+new Viewer()
