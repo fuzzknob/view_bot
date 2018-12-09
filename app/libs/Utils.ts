7677faf76b6707469c195944ba56bfda7b67e1d9
Gagan Rai <gagan_rai@outlook.com>
2018-12-09 22:02:06 +0545

    add check for  null target

--- app/libs/Utils.ts
+++ app/libs/Utils.ts
@@ -1,12 +1,15 @@
 class Utils {
 
     public static fireEvent(target: Element, eventName: string): boolean {
+        if (target) {
+            return false
+        }
         let event = new Event(eventName)
         return target.dispatchEvent(event)
     }
 
-    public static performClick(target: Element): void {
-        this.fireEvent(target, 'click')
+    public static performClick(target: Element): boolean {
+        return this.fireEvent(target, 'click')
     }
 
     public static select(selector: string): HTMLElement {
